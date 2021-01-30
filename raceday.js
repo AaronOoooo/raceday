2624332beba70d9aa9f5238e5129ae2cd35a080c
AaronOoooo <aaron.o.hall@gmail.com>
2021-01-30 16:34:49 -0600

    Completed

--- raceday.js
+++ raceday.js
@@ -10,20 +10,25 @@ let raceNumber = Math.floor(Math.random()*1000)
 console.log(raceNumber);
 
 let regEarly = false;
-let runnerAge = 13;
+let runnerAge = 18;
 
 if (regEarly === true && runnerAge > 18 ) {
     raceNumber += 1000;
 }
 
-if (runnerAge > 18 && regEarly) {
-    console.log(`Runner ${raceNumber} will start at 9:30 AM`);
+if (runnerAge === 18) { 
+    console.log(`Runner ${raceNumber} please visit the front desk.`)
 } else {
-    if (runnerAge > 18 && !regEarly) {
-        console.log(`Runner ${raceNumber} will start at 11:00 AM`);
+    if (runnerAge > 18 && regEarly) {
+    console.log(`Runner ${raceNumber} will start at 9:30 AM`);
     } else {
-        console.log(`Runner ${raceNumber} will start at 12:30 PM`)
+        if (runnerAge > 18 && !regEarly) {
+            console.log(`Runner ${raceNumber} will start at 11:00 AM`);
+        } else {
+            console.log(`Runner ${raceNumber} will start at 12:30 PM`)
+        }
     }
+
 }
 
 //console.log(raceNumber);
